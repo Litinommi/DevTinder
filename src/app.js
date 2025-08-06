ec24814793b1833ab6433e68199a55a46c653391
Litinommi <litin.ommi@gmail.com>
2025-08-06 21:31:09 +0530

    feat(server): add basic Express server with /text route

---
+++ src/app.js
@@ -0,0 +1,13 @@
+// Import the Express framework
+const express = require("express");
+
+// Create an instance of an Express application
+const app = express();
+
+app.use("/text",(req,re)=>{
+    re.send("welcome World!!!!!!!!!!!!");
+})
+
+app.listen(7777,()=>{
+    console.log("server is listening");
+})
