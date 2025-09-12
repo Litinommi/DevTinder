de3f9bbda23e2ed4583f3cd168fab4d6561a1009
Litinommi <litin.ommi@gmail.com>
2025-09-12 08:13:13 +0530

    data sanitization

--- src/app.js
+++ src/app.js
@@ -17,6 +17,85 @@ app.post("/signup",async(req,res)=>{
     }
 })
 
+app.get("/getemailID",async(req,res)=>{
+    try{
+        const userEmail = await User.find(req.body)
+        res.send(userEmail);
+    }
+    catch{
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.get("/findByID",async(req,res)=>{
+    try{
+        const userIddata = await User.find(req.body)
+        res.send(userIddata);
+    }
+    catch{
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.get("/feed",async(req,res)=>{
+    try{
+        const feedData = await User.find({});
+        console.log(feedData)
+        if(feedData.length>0){
+            res.send(feedData);
+        }
+        else{
+            res.send("no data");
+        }
+    }
+    catch{
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.delete("/deleteUser",async(req,res)=>{
+    try{
+        const deleteUser = await User.findByIdAndDelete(req.body);
+        res.send(deleteUser);
+    }
+    catch{
+        res.status(404).send("Something went wrong");
+    }
+})
+
+app.patch("/updateUser/:userId",async(req,res)=>{
+    const userId = req.params?.userId;
+    const data = req.body;
+    try{
+
+        const ALLOWED_UPDATES = ['firstName','secondName','about','skills','password'];
+
+        const CanUpdate = Object.keys(data).every((k)=>
+            ALLOWED_UPDATES.includes(k)
+        )
+
+        if(!CanUpdate){
+            throw new Error("Field not found");
+        }
+
+        if(data.skills.length>10){
+             throw new Error("skills cannot be entered more than 10")
+        }
+
+
+        const updateprofile = await  User.findByIdAndUpdate(userId,data,{
+            runValidators:true
+        })
+
+        res.send("User updated successfully");
+
+
+    }
+    catch(err){
+        res.status(404).send("Something went wrong "+err.message);
+    }
+})
+
 dbConnect().then(()=>{
     console.log("database is connected")
     app.listen(7777,()=>{
--- src/models/user.js
+++ src/models/user.js
@@ -2,18 +2,43 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
     "firstName":{
-        type : String
+        type : String,
+        minLength : 4,
+        maxLength : 50
     },
     "secondName":{
         type : String,
+        minLength : 4,
+        maxLength : 50
     },
     "emailId":{
         type:String,
+        unique : true,
+        trim: true
     },
+    "gender":{
+        type: String,
+        validate(value){
+            if(!['male','female','others'].includes(value)){
+                throw new Error("Gender is not valid");
+                }
+            }
+        },
+    
     "password":{
-        type:String,
+        type : String,
+        minLength: 8
     },
-})
+    "about":{
+        type:String,
+        default:"Hi"
+    },
+    "skills":{
+        type:[String]
+    }
+    
+},
+    {"timestamps":true})
 
 const userModel = mongoose.model("User",userSchema);
 
