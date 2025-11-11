13203c5a7a6dcd46000fc71d85943934ebd7fca4
Litinommi <litin.ommi@gmail.com>
2025-11-11 22:27:30 +0530

    added connection request apis

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,16 +1,75 @@
 const express = require("express");
 const requestRoute = express.Router();
 const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest")
+const User = require("../models/user");
 
 
-requestRoute.post("/sentConnectionRequest", userAuth ,async(req,res)=>{
+requestRoute.post("/request/review/:status/:requestId",userAuth,async(req,res)=>{
     try{
-        const user = req.user;
-        res.send(user.firstName+" sent the request");
+        const loggedInUser = req.user;
+        const {status,requestId} = req.params;
+
+        const allowedStatus = ["accepted","ignored"];
+
+        if(!allowedStatus.includes(status)){
+            res.status(400).json({message:"Status not allowed!!"});
+        }
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId:loggedInUser._id,
+            status:"interested",
+        })
+
+        if(!connectionRequest){
+            return res.status(404).json({message:"Connection not found!!"})
+        }
+        connectionRequest.status = status;
+        const data = await connectionRequest.save();
+        res.json({message:"Connection Request "+ status,data})
     }
     catch(err){
-        res.status(400).send("Please login once again"+ err.message);
+        res.status(400).send("Error: "+ err.message);
     }
 })
+
+requestRoute.post("/request/send/:status/:toUserId",userAuth,async(req,res)=>{
+    try{
+        const fromUser = req.user;
+        const fromUserId = fromUser._id;
+        const toUserId = req.params.toUserId;
+        const status = req.params.status;
+
+        const toUser =await User.findOne({_id:toUserId});
+        const allowedStatus = ["rejected","interested"];
+        if(!allowedStatus.includes(status)){
+            throw new Error("Wrong connection request");
+        }
+        if(!toUser){
+            throw new Error("The connection you sent doesn't exist");
+        }
+        const isRequestAlreadyExists = await ConnectionRequest.findOne({
+            $or:[
+                {fromUserId,toUserId},
+                {fromUserId:toUserId,toUserId:fromUserId}
+            ]
+        })
+
+        if(isRequestAlreadyExists){
+            throw new Error("The connection you sent already exist");
+        }
+
+        const newConnectionRequest = new ConnectionRequest({fromUserId,toUserId,status})
+        await newConnectionRequest.save();
+        res.send(fromUser.firstName+" sent the request");
+    }
+    catch(err){
+        res.status(400).send("Error: "+ err.message);
+    }
+})
+
+
+
+
 
 module.exports = requestRoute;
